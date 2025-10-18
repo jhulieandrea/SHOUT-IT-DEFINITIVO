8926fdb09cb1d9b07326191ea8236703cf180a3d
jhulieandrea <jhulieandrea@gmail.com>
2025-10-18 13:22:18 -0500

    Se actualizo ruta_atencion para que las imagees las reconozca desde el server.js

--- server/server.js
+++ server/server.js
@@ -22,12 +22,12 @@ const tiposViolencia = [
 const rutasAtencion = [
   { id: 1, nombre: "Policía Nacional", imagen: "/imagenes/entidades_rutas_atencion/policia_nacional.png" },
   { id: 2, nombre: "Medicina Legal", imagen: "/imagenes/entidades_rutas_atencion/medicina_legal.png" },
-  { id: 3, nombre: "Fiscalía General de la Nación", imagen: "/imagenes/entidades_rutas_atencion/fiscalia.png" },
+  { id: 3, nombre: "Fiscalía General de la Nación", imagen: "/imagenes/entidades_rutas_atencion/fiscalia_general.png" },
   { id: 4, nombre: "Defensoría del Pueblo", imagen: "/imagenes/entidades_rutas_atencion/defensoria.png" },
   { id: 5, nombre: "Bienestar Familiar", imagen: "/imagenes/entidades_rutas_atencion/bienestar_familiar.png" },
   { id: 6, nombre: "Línea Púrpura", imagen: "/imagenes/entidades_rutas_atencion/linea_purpura.png" },
   { id: 7, nombre: "ICBF", imagen: "/imagenes/entidades_rutas_atencion/icbf.png" },
-  { id: 8, nombre: "Secretaría Distrital de la Mujer", imagen: "/imagenes/entidades_rutas_atencion/secretaria_mujer.png" },
+  { id: 8, nombre: "Secretaría Distrital de la Mujer", imagen: "/imagenes/entidades_rutas_atencion/secretaria_distrital_mujer.png" },
   { id: 9, nombre: "Casas de Justicia", imagen: "/imagenes/entidades_rutas_atencion/casas_justicia.png" },
   { id: 10, nombre: "Comisarías de Familia", imagen: "/imagenes/entidades_rutas_atencion/comisarias_familia.png" }
 ];
